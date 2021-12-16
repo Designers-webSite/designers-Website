e3fa26c90f5eb80773ccb2c05185a7ed30f3e6f3
thuriabdullah <thiii302081@gmail.com>
2021-12-16 09:27:50 +0300

    add design home page

--- Froont/design/src/App.js
+++ Froont/design/src/App.js
@@ -4,23 +4,31 @@ import Register from './Components/Register';
 import Utility from './Components/Utility';
 import CreateDesigner from './Components/CreateDesigner';
 import Gallery from './Components/Gallery';
+import DesignType from './Components/DesignType';
+import Home from './Components/Home';
 import NavBar from './Components/NavBar';
+import Login from './Components/Login';
 function App() {
   return (
     <BrowserRouter>
-    <NavBar/>
-      <Routes>
-        
+      {/* <Routes>
+        <Route exact path="/" />
         <Route path="/register" element={<Register/>} />
         <Route path="/utility" element={<Utility/>} />
         <Route path="/createDesigner" element={<CreateDesigner/>} />
         <Route path="/gallery" element={<Gallery/>} />
-       
+        <Route path="/designType" element={<DesignType/>} /> */}
 
-        
+        <NavBar/>
+				<Routes>
+					<Route path="/" element={<Home />} />
+		
+					<Route path="/designers" element={<Home />} />
+					<Route path="/support" element={<Home />} />
+          <Route path="/CreateDesigner" element={<CreateDesigner />} />
+          <Route path="/register" element={<Register />} />
 				</Routes>
         
-   
     </BrowserRouter>
   );
 }
---
+++ Froont/design/src/Components/Home.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { FaFacebook, FaInstagram, FaTwitter, FaBehance } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+    return (
+        <div className='content home'>
+            <div className="container">
+                <div className="page-top">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <div className="text">
+                                <h1 className='mb-2'>Best Services from Best Designers in Arabic World</h1>
+                                <h3 className='mb-3'>Fint Best Design Services</h3>
+                                <div className="search-box my-4">
+                                    <input type="text" placeholder='Find Design Services . . .' />
+                                    <button>search</button>
+                                </div>
+                                <div className="links">
+                                    <Link to="/CreateDesigner" className='main-btn'>Find Job</Link>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="images">
+                                <img src="./images/img1.jpg" alt="" className="main-img" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="ex-design">
+                <div className="text-card">
+                        <h4>External Design</h4>
+                     </div> 
+                     </div>
+                     <div className="in-design">
+                     <div className="text-card right">
+                        <h4>Interior Design</h4> 
+                        </div>
+                        </div> 
+
+
+                
+
+                <footer>
+                    <h2>Contact Us</h2>
+                    <div className="social-icons">
+                        <FaFacebook />
+                        <FaInstagram />
+                        <FaTwitter />
+                        
+                    </div>
+                </footer>
+            </div>
+        </div>
+    )
+}
+
+export default Home
